8eabc7b8574d4d1e62096badbc2a67366d53b3fc
Joy <dharmeshrohit.online@gmail.com>
2025-07-19 23:51:42 +0530

    Created products table in bronze layer

--- warehouse/scripts/bronze/ddl_bronze.sql
+++ warehouse/scripts/bronze/ddl_bronze.sql
@@ -3,3 +3,12 @@ Factory		varchar(50),
 Latitude	float,
 Longitude	float,
 )
+
+create table bronze.products(
+Division		varchar(50),
+Product_Name	varchar(50),
+Factory			varchar(50),
+Product_ID		varchar(50),
+Unit_Price		float,
+Unit_Cost		float,
+)
