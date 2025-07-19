8b8b9f337301f36c5734db93b8d00fbfaf19fd47
Joy <dharmeshrohit.online@gmail.com>
2025-07-20 00:16:48 +0530

    Created sales table in bronze layer

--- warehouse/scripts/bronze/ddl_bronze.sql
+++ warehouse/scripts/bronze/ddl_bronze.sql
@@ -12,3 +12,25 @@ Product_ID		varchar(50),
 Unit_Price		float,
 Unit_Cost		float,
 )
+
+
+create table bronze.sales(
+Row_ID		int,
+Order_ID	varchar(50),
+Order_Date	date,
+Ship_Date	date,
+Ship_Mode	varchar(50),
+Customer_ID	int,
+Country		varchar(50),
+City		varchar(50),
+State_Province	varchar(50),
+Postal_Code		varchar(50),
+Division		varchar(50),
+Region			varchar(50),
+Product_Id		varchar(50),
+Product_Name	varchar(50),
+Sales			float,
+Units			int,
+Gross_Profit	float,
+Cost			float,
+)
