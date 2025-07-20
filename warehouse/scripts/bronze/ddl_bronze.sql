cf21faee96795bbd56fa421d3823d7c3aa6de380
Joy <dharmeshrohit.online@gmail.com>
2025-07-20 15:24:05 +0530

    added description & formatted the queries

--- warehouse/scripts/bronze/ddl_bronze.sql
+++ warehouse/scripts/bronze/ddl_bronze.sql
@@ -1,63 +1,96 @@
-create table bronze.factories(
-Factory		varchar(50),
-Latitude	float,
-Longitude	float,
-)
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
 
-create table bronze.products(
-Division		varchar(50),
-Product_Name	varchar(50),
-Factory			varchar(50),
-Product_ID		varchar(50),
-Unit_Price		float,
-Unit_Cost		float,
-)
-
-
-create table bronze.sales(
-Row_ID		int,
-Order_ID	varchar(50),
-Order_Date	date,
-Ship_Date	date,
-Ship_Mode	varchar(50),
-Customer_ID	int,
-Country		varchar(50),
-City		varchar(50),
-State_Province	varchar(50),
-Postal_Code		varchar(50),
-Division		varchar(50),
-Region			varchar(50),
-Product_Id		varchar(50),
-Product_Name	varchar(50),
-Sales			float,
-Units			int,
-Gross_Profit	float,
-Cost			float,
-)
+-- Create the factories table
+IF OBJECT_ID('bronze.factories', 'U') IS NOT NULL
+    DROP TABLE bronze.factories;
+GO
+CREATE TABLE bronze.factories (
+    Factory   VARCHAR(50),
+    Latitude  FLOAT,
+    Longitude FLOAT
+);
 
 
-create table bronze.targets(
-Division	varchar(50),
-Target		int,
-)
+-- Create the products table
+IF OBJECT_ID('bronze.products', 'U') IS NOT NULL
+    DROP TABLE bronze.products;
+GO
+CREATE TABLE bronze.products (
+    Division      VARCHAR(50),
+    Product_Name  VARCHAR(50),
+    Factory       VARCHAR(50),
+    Product_ID    VARCHAR(50),
+    Unit_Price    FLOAT,
+    Unit_Cost     FLOAT
+);
 
-create table bronze.uszips(
-zip		varchar(50),
-lat		float,
-lng		float,
-city	varchar(50),
-state_id	varchar(50),
-state_name	varchar(50),
-zcta	varchar(50),
-parent_zcta	varchar(50),
-population	int,
-density		float,
-county_fips	int,
-county_name	varchar(50),
-county_weights varchar(100),
-county_names_all	varchar(100),
-county_fips_all		varchar(100),
-imprecise	varchar(50),
-military	varchar(50),
-timezone	varchar(50),
-)
+
+-- Create the sales table
+IF OBJECT_ID('bronze.sales', 'U') IS NOT NULL
+    DROP TABLE bronze.sales;
+GO
+CREATE TABLE bronze.sales (
+    Row_ID          INT,
+    Order_ID        VARCHAR(50),
+    Order_Date      DATE,
+    Ship_Date       DATE,
+    Ship_Mode       VARCHAR(50),
+    Customer_ID     INT,
+    Country         VARCHAR(50),
+    City            VARCHAR(50),
+    State_Province  VARCHAR(50),
+    Postal_Code     VARCHAR(50),
+    Division        VARCHAR(50),
+    Region          VARCHAR(50),
+    Product_Id      VARCHAR(50),
+    Product_Name    VARCHAR(50),
+    Sales           FLOAT,
+    Units           INT,
+    Gross_Profit    FLOAT,
+    Cost            FLOAT
+);
+
+
+-- Create the targets table
+IF OBJECT_ID('bronze.targets', 'U') IS NOT NULL
+    DROP TABLE bronze.targets;
+GO
+CREATE TABLE bronze.targets (
+    Division VARCHAR(50),
+    Target   INT
+);
+
+
+-- Create the uszips table
+IF OBJECT_ID('bronze.uszips', 'U') IS NOT NULL
+    DROP TABLE bronze.uszips;
+GO
+CREATE TABLE bronze.uszips (
+    zip                 VARCHAR(50),
+    lat                 FLOAT,
+    lng                 FLOAT,
+    city                VARCHAR(50),
+    state_id            VARCHAR(50),
+    state_name          VARCHAR(50),
+    zcta                VARCHAR(50),
+    parent_zcta         VARCHAR(50),
+    population          INT,
+    density             FLOAT,
+    county_fips         INT,
+    county_name         VARCHAR(50),
+    county_weights      VARCHAR(100),
+    county_names_all    VARCHAR(100),
+    county_fips_all     VARCHAR(100),
+    imprecise           VARCHAR(50),
+    military            VARCHAR(50),
+    timezone            VARCHAR(50)
+);
