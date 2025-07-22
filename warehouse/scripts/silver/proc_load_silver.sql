44416d81e1a2ffd33145572991b4ef8ab0f8d5e3
Joy <dharmeshrohit.online@gmail.com>
2025-07-22 15:36:02 +0530

    Inserted uszips table into silver schmea from the bronze schmea & Formatted the queries

--- warehouse/scripts/silver/proc_load_silver.sql
+++ warehouse/scripts/silver/proc_load_silver.sql
@@ -1,84 +1,119 @@
-insert into silver.factories(
-factory,
-latitude,
-longitude
+INSERT INTO silver.factories (
+    factory,
+    latitude,
+    longitude
 )
+SELECT 
+    TRIM(Factory) AS factory,
+    Latitude,
+    Longitude
+FROM bronze.factories;
 
-select
-trim(Factory),
-Latitude,
-Longitude
-from bronze.factories
-
-
-insert into silver.products(
-division,
-product_name,
-factory,
-product_id,
-Unit_Price,
-Unit_cost
+INSERT INTO silver.products (
+    division,
+    product_name,
+    factory,
+    product_id,
+    Unit_Price,
+    Unit_cost
 )
+SELECT 
+    TRIM(Division) AS division,
+    TRIM(product_name) AS product_name,
+    TRIM(factory) AS factory,
+    TRIM(product_id) AS product_id,
+    Unit_Price,
+    Unit_cost
+FROM bronze.products;
 
-select
-trim(Division) as division,
-trim(product_name) as product_name,
-trim(factory) factory,
-TRIM(product_id) product_id,
-Unit_Price,
-Unit_cost
-from bronze.products
-
-
-insert into silver.sales (
-row_id,
-order_id,
-order_date,
-ship_date,
-ship_mode,
-customer_id,
-country,
-city,
-state_province,
-postal_code,
-division,
-region,
-product_id,
-product_name,
-sales,
-units,
-gross_profit,
-cost)
-
-select 
-row_id,
-order_id,
-order_date,
-ship_date,
-trim(ship_mode) as Ship_Mode,
-Customer_ID,
-trim(country) as Country,
-trim(city) as City,
-trim(state_province) as State_Province,
-Postal_Code,
-trim(Division) as Division,
-trim(Region) as Region,
-Product_Id,
-trim(Product_Name) as Product_Name,
-Sales,
-Units,
-Gross_Profit,
-Cost
-from bronze.sales
-order by Row_ID asc
-
-
-insert into silver.targets(
-division,
-target
+INSERT INTO silver.sales (
+    row_id,
+    order_id,
+    order_date,
+    ship_date,
+    ship_mode,
+    customer_id,
+    country,
+    city,
+    state_province,
+    postal_code,
+    division,
+    region,
+    product_id,
+    product_name,
+    sales,
+    units,
+    gross_profit,
+    cost
 )
+SELECT 
+    row_id,
+    order_id,
+    order_date,
+    ship_date,
+    TRIM(ship_mode) AS ship_mode,
+    Customer_ID,
+    TRIM(country) AS country,
+    TRIM(city) AS city,
+    TRIM(state_province) AS state_province,
+    Postal_Code,
+    TRIM(Division) AS division,
+    TRIM(Region) AS region,
+    Product_Id,
+    TRIM(Product_Name) AS product_name,
+    Sales,
+    Units,
+    Gross_Profit,
+    Cost
+FROM bronze.sales
+ORDER BY Row_ID ASC;
 
-select
-trim(division) as division,
-target
-from bronze.targets
+INSERT INTO silver.targets (
+    division,
+    target
+)
+SELECT 
+    TRIM(division) AS division,
+    target
+FROM bronze.targets;
+
+INSERT INTO silver.uszips (
+    zip,
+    lat,
+    lng,
+    city,
+    state_id,
+    state_name,
+    zcta,
+    parent_zcta,
+    population,
+    density,
+    county_fips,
+    county_name,
+    county_weights,
+    county_names_all,
+    county_fips_all,
+    imprecise,
+    military,
+    timezone
+)
+SELECT 
+    zip,
+    lat,
+    lng,
+    TRIM(city) AS city,
+    TRIM(state_id) AS state_id,
+    TRIM(state_name) AS state_name,
+    TRIM(zcta) AS zcta,
+    COALESCE(parent_zcta, 'n/a') AS parent_zcta,
+    population,
+    density,
+    county_fips,
+    TRIM(county_name) AS county_name,
+    TRIM(county_weights) AS county_weights,
+    TRIM(county_names_all) AS county_names_all,
+    county_fips_all,
+    TRIM(imprecise) AS imprecise,
+    TRIM(military) AS military,
+    TRIM(timezone) AS timezone
+FROM bronze.uszips;
