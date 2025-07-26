3e95aecd12cb962104889b6ecd9bd3d747438a6f
Joy <dharmeshrohit.online@gmail.com>
2025-07-26 14:50:07 +0530

    Check Data Consistency: Quantity * Price

--- warehouse/tests/quality_checks_silver.sql
+++ warehouse/tests/quality_checks_silver.sql
@@ -23,3 +23,32 @@ SELECT
     unit_price 
 FROM silver.products
 WHERE unit_price < 0 OR unit_price IS NULL;
+
+
+-- ====================================================================
+-- Checking 'silver.sales'
+-- ====================================================================
+-- Check for Invalid Date Orders (Order Date > Shipping/Due Dates)
+-- Expectation: No Results
+SELECT 
+    * 
+FROM silver.sales
+WHERE order_date > ship_date 
+
+-- Check Data Consistency: Sales = Quantity * Price
+-- Expectation: No Results
+SELECT DISTINCT 
+    s.sales,
+    s.units,
+    p.unit_price 
+FROM silver.sales s
+LEFT JOIN silver.products p
+on s.product_id = p.product_id
+WHERE sales != (s.units * p.unit_price) 
+   OR s.sales IS NULL 
+   OR s.Units IS NULL 
+   OR p.unit_price IS NULL
+   OR s.sales <= 0 
+   OR s.units <= 0 
+   OR p.unit_price <= 0
+ORDER BY s.sales, s.units, p.unit_price;
