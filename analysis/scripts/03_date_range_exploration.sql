cf3d6b7a11f610f9fb54406b3fe686c50e5981be
Joy <dharmeshrohit.online@gmail.com>
2025-07-28 13:57:45 +0530

    Date Range Exploration

---
+++ analysis/scripts/03_date_range_exploration.sql
@@ -0,0 +1,19 @@
+/*
+===============================================================================
+Date Range Exploration 
+===============================================================================
+Purpose:
+    - To determine the temporal boundaries of key data points.
+    - To understand the range of historical data.
+
+SQL Functions Used:
+    - MIN(), MAX(), DATEDIFF()
+===============================================================================
+*/
+
+-- Determine the first and last order date and the total duration in months
+SELECT 
+    MIN(order_date) AS first_order_date,
+    MAX(order_date) AS last_order_date,
+    DATEDIFF(MONTH, MIN(order_date), MAX(order_date)) AS order_range_months
+FROM gold.fact_sales;
